a1b3917f9c7884ec650d665832c78029c257b9ac
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-07-19 15:17:35 -0400

    setting up some global variables

--- chessbase.py
+++ chessbase.py
@@ -1,4 +1,11 @@
 """
-this module contains the chess piece class and the board setup function
+this module contains the board setup function and board draw function
 running this module on its own plays a game of standard chess
 """
+
+# the board Array which will hold our piece information
+board = []
+# the subsurface array for our chess piece images
+piece_sheet = None
+# a lookup list for chess piece names
+piece_names = ['king','queen','bishop','rook','knight','pawn']
